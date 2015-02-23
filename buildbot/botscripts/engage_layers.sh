be3b3ee936b8c6d12fc5fb336afd4ddadf9736f7
Chris Rogers <rogersc@ainfosec.com>
2015-02-23 15:24:54 -0500

    add support for specifying layer branch to build from

--- buildbot/botscripts/engage_layers.sh
+++ buildbot/botscripts/engage_layers.sh
@@ -11,14 +11,15 @@ IFS=','
 #sed -i "/^$/d" build/conf/bblayers.conf
 head build/conf/bblayers.conf -n -3 > build/conf/bblayers.conf.tmp
 mv build/conf/bblayers.conf.tmp build/conf/bblayers.conf
-for pair in $LAYERS;
+for trip in $LAYERS;
 do
-	LAYER_NAME=`echo $pair | cut -f 1 -d ':'`
-	LAYER_REPO=`echo $pair | cut -f 2 -d ':'`	
+	LAYER_NAME=`echo $trip | cut -f 1 -d ':'`
+	LAYER_REPO=`echo $trip | cut -f 2 -d ':'`
+	LAYER_BRANCH=`echo $trip | cut -f 3 -d ':'`	
 	echo "  \${TOPDIR}/repos/$LAYER_NAME \\" >> build/conf/bblayers.conf
 	
 	cd build/repos
-	git clone git://$LAYER_REPO
+	git clone -b $LAYER_BRANCH git://$LAYER_REPO
 	cd ../../
 	
 done
